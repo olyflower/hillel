33135d42faee72f2ac1046061ed5ab77536b04bc
olyflower <2013.olga.yudina@gmail.com>
2022-08-31 13:49:37 +0300

    lesson_03_hw_02

---
+++ lesson_03_hw_02/3.py
@@ -0,0 +1,10 @@
+# Написати програму, що перетворює значення рядкової змінної з формату snake_case в формат
+# CapitalizedWords (a.k.a. Capitalized camelCase). Значення змінної отримуйте з input().
+# Для простоти вважаємо, що значення змінної завжди складається з 3-х слів.
+# Наприклад: 'employee_first_name' -> 'EmployeeFirstName'.
+
+a = input('Введите переменную в формате snake_case (3 слова): ')
+c = a.title()
+b = c.split('_')
+result = b[0] + b[1] + b[2]
+print(result)
---
+++ lesson_03_hw_02/3_1.py
@@ -0,0 +1,10 @@
+# Написати програму, що перетворює значення рядкової змінної з формату snake_case
+# в формат CapitalizedWords (a.k.a. Capitalized camelCase).
+# Значення змінної отримуйте з input(). Значення змінної складається з будь-якої кількості слів.
+
+a = input('Введите переменную в формате snake_case: ')
+b = a.split('_')
+c = map(str.capitalize, b)
+capitalized = ''.join(c)
+print(capitalized)
+
